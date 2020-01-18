04b061843709e0b980080c4d44867399c5a20d7b
liuyanan920921 <tina.liu0921@aliyun.com>
2020-01-18 20:11:38 +0800

    first commmit

---
+++ src/main/java/com/example/demo/SpringCloud0113Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloud0113Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloud0113Application.class, args);
+	}
+
+}
